b5c255fc8307bf129fea80a75276b873a3f409d2
RuSignalFlag <1848789165@qq.com>
2023-05-15 20:34:33 +0800

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   .env.template 	new file:   api.py 	new file:   requirements.txt 	new file:   web.py

---
+++ api.py
@@ -0,0 +1,154 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from transformers import AutoTokenizer, AutoModel
+import uvicorn, json, datetime
+import torch
+from sse_starlette.sse import EventSourceResponse
+from pydantic import BaseModel
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Pinecone
+from langchain.document_loaders import TextLoader
+import pinecone
+import os.path
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())
+
+DEVICE = "cuda"
+DEVICE_ID = "0"
+CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE
+emdedding_model_name = os.getenv("emdedding_model_name")
+pinecone_api_key = os.getenv("pinecone_api_key")
+pinecone_environment = os.getenv("pinecone_environment")
+
+app = FastAPI()
+
+def torch_gc():
+    if torch.cuda.is_available():
+        with torch.cuda.device(CUDA_DEVICE):
+            torch.cuda.empty_cache()
+            torch.cuda.ipc_collect()
+
+class Params(BaseModel):
+    prompt: str = '你好'
+    history: list[list[str]] = []
+    max_length: int = 2048
+    top_p: float = 0.7
+    temperature: float = 0.95
+    index_name: str = 'test'
+
+
+async def create_chat(params: Params):
+    global model , tokenizer
+    response,history = model.chat(tokenizer,
+                                  params.prompt,
+                                  history=params.history,
+                                  max_length=params.max_length,
+                                  top_p=params.top_p,
+                                  temperature=params.temperature)
+    now = datetime.datetime.now()
+    time = now.strftime("%Y-%m-%d %H:%M:%S")
+    answer = {
+        "response": response,
+        "history": history,
+        "status": 200,
+        "time": time
+    }
+    log = "[" + time + "] " + '", prompt:"' + params.prompt + '", response:"' + repr(response) + '"'
+    print(log)
+    torch_gc()
+    return answer
+
+async def create_chat_stream(params: Params):
+    global model , tokenizer
+    for response,history in model.stream_chat(tokenizer,
+                                  params.prompt,
+                                  history=params.history,
+                                  max_length=params.max_length,
+                                  top_p=params.top_p,
+                                  temperature=params.temperature):
+        now = datetime.datetime.now()
+        time = now.strftime("%Y-%m-%d %H:%M:%S")
+        answer = {
+        "response": response,
+        "history": history,
+        "status": 200,
+        "time": time
+        }
+        yield json.dumps(answer, ensure_ascii=False)
+    log = "[" + time + "] " + '", prompt:"' + params.prompt + '", response:"' + repr(response) + '"'
+    print(log)
+    torch_gc()
+
+
+@app.post("/chat")
+async def post_chat(params: Params):
+    answer = await create_chat(params)
+    return answer
+
+@app.post("/chat_stream")
+async def post_chat_stream(params: Params):
+    return EventSourceResponse(create_chat_stream(params))
+
+@app.post("/searchvectorsbase")
+async def get_similarity_answer (params: Params):
+    global embeddings
+    index_name = params.index_name
+    query = params.prompt
+    docsearch = Pinecone.from_existing_index(index_name, embeddings)
+    results = docsearch.similarity_search(query, k=2)
+    print(results)
+    docs = [results[i].page_content for i in range(len(results))]
+    content = {
+        "results": docs
+    }
+    torch_gc()
+    now = datetime.datetime.now()
+    time = now.strftime("%Y-%m-%d %H:%M:%S")
+    log = "[" + time + "] " + "query: "+ " " +str(query) +" searchvectorsbase: " + " " + str(content)
+    print(log)
+    return JSONResponse(status_code=200, content=content) 
+
+@app.post("/embeddings")
+async def embeddings_text (file: UploadFile = File(...)):
+  global embeddings, pinecone
+  content = await file.read()
+  target_directory = os.path.dirname(__file__)  # the path of this program
+  target_directory = os.path.join(target_directory, "knowledgeBase")  # build the path of the vector knowledge base
+  target_file_path = os.path.join(target_directory, file.filename)  # build the path of the file
+  with open(target_file_path, "wb") as f:
+        f.write(content)
+  print(os.path.splitext(file.filename)[0])
+  index_name = os.path.splitext(file.filename)[0]
+  indexes = pinecone.list_indexes()  #  delete existed indexes
+  if len(indexes) != 0:
+            for index in indexes:
+                pinecone.delete_index(index)
+  pinecone.create_index(index_name, dimension=1024, metric="euclidean")       
+  loader = TextLoader(target_file_path,encoding="utf-8")
+  documents = loader.load()
+  text_splitter = CharacterTextSplitter(chunk_size=256, chunk_overlap=0)
+  docs = text_splitter.split_documents(documents)
+  docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+  message = {
+      "base_name": index_name
+  }
+  torch_gc()
+  now = datetime.datetime.now()
+  time = now.strftime("%Y-%m-%d %H:%M:%S")
+  log = "[" + time + "] " + str(file.filename) + " hac been uploaded to " + str(index_name) + " index" + "\n"
+  print(log)
+  return JSONResponse(status_code=200,content=message)
+    
+if __name__ == '__main__':
+    tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
+    model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
+    model.eval()
+    embeddings = HuggingFaceEmbeddings(model_name=emdedding_model_name, model_kwargs={'device': CUDA_DEVICE})
+    pinecone.init(
+        api_key=pinecone_api_key,  # find at app.pinecone.io
+        environment=pinecone_environment  # next to api key in console
+    )
+    uvicorn.run(app, host='0.0.0.0', port=8000, workers=1)
+    
---
+++ web.py
@@ -0,0 +1,183 @@
+import datetime
+import gradio as gr
+import mdtex2html
+
+import requests
+import json
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())
+
+chatglm_endpoint_chat = os.getenv("chatglm_endpoint_chat")
+chatglm_endpoint_chat_stream = os.getenv("chatglm_endpoint_chat_stream")
+emdbeddings_endpoint = os.getenv("emdbeddings_endpoint")
+searchvectorsbase_endpoint = os.getenv("searchvectorsbase_endpoint")
+prompt_template = """已知信息：
+{context} 
+
+根据上述已知信息，简洁和专业的来回答用户的问题。如果无法从中得到答案，请说 “根据已知信息无法回答该问题” 或 “没有提供足够的相关信息”，不允许在答案中添加编造成分，答案请使用中文。 问题是：{question}"""
+
+
+"""Override Chatbot.postprocess"""
+
+
+def postprocess(self, y):
+    if y is None:
+        return []
+    for i, (message, response) in enumerate(y):
+        y[i] = (
+            None if message is None else mdtex2html.convert((message)),
+            None if response is None else mdtex2html.convert(response),
+        )
+    return y
+
+
+gr.Chatbot.postprocess = postprocess
+
+
+def parse_text(text):
+    lines = text.split("\n")
+    lines = [line for line in lines if line != ""]
+    count = 0
+    for i, line in enumerate(lines):
+        if "```" in line:
+            count += 1
+            items = line.split('`')
+            if count % 2 == 1:
+                lines[i] = f'<pre><code class="language-{items[-1]}">'
+            else:
+                lines[i] = f'<br></code></pre>'
+        else:
+            if i > 0:
+                if count % 2 == 1:
+                    line = line.replace("`", "\`")
+                    line = line.replace("<", "&lt;")
+                    line = line.replace(">", "&gt;")
+                    line = line.replace(" ", "&nbsp;")
+                    line = line.replace("*", "&ast;")
+                    line = line.replace("_", "&lowbar;")
+                    line = line.replace("-", "&#45;")
+                    line = line.replace(".", "&#46;")
+                    line = line.replace("!", "&#33;")
+                    line = line.replace("(", "&#40;")
+                    line = line.replace(")", "&#41;")
+                    line = line.replace("$", "&#36;")
+                lines[i] = "<br>"+line
+    text = "".join(lines)
+    return text
+
+def predict(input, chatbot, max_length, top_p, temperature, history, conversation_turn, stream, vector_base, index_name):
+    chatbot.append((parse_text(input),""))
+    prompt = input
+    if vector_base:
+        url = searchvectorsbase_endpoint
+        headers_v = {'Content-Type': 'application/json'}
+        data_v = {"prompt": input,"index_name": index_name}
+        data_v = json.dumps(data_v)
+        response = requests.post(url=url,headers=headers_v,data=data_v)
+        if response.status_code == 200:
+            result = json.loads(response.text)
+            result = result["results"]
+            context = "\n".join([doc for doc in result])
+            prompt = prompt_template.replace("{question}", input).replace("{context}", context)
+        else:
+            prompt = input
+    else:
+        prompt = input
+    headers = {'Content-Type': 'application/json'}
+    history = [] if len(history) == conversation_turn else history
+    data = {"prompt": prompt,"history": history,"max_length": max_length,"top_p": top_p,"temperature": temperature}
+    if stream:
+        res = requests.post(url=chatglm_endpoint_chat_stream,headers=headers,data=json.dumps(data),stream=True)
+    else:
+        res = requests.post(url=chatglm_endpoint_chat,headers=headers,data=json.dumps(data))
+    for item in res.iter_lines():
+        item = item.decode("utf-8")
+        if item.strip() == '':
+            continue 
+        if stream:     
+            item = item[6:]
+        else:
+            item = item
+        try:
+            item = json.loads(item)
+        except:
+            continue   
+        response = item["response"]
+        history = item["history"]
+        chatbot[-1] = (parse_text(input),parse_text(response))       
+        yield chatbot, history
+    now = datetime.datetime.now()
+    time = now.strftime("%Y-%m-%d %H:%M:%S")
+    log = "[" + time + "] " + '", prompt:"' + input + '", prompt_context:"' + prompt +'", response:"' + repr(response) + '"' + ", stream:" + str(stream) + '"' +"\n"
+    print(log)
+
+
+def get_vector_base(File, chatbot):
+    if File is None:
+        return
+    url = emdbeddings_endpoint
+    file = {'file': open(File.name, 'rb')}
+    response = requests.post(url, files=file)
+    if response.status_code == 200:
+        result = json.loads(response.text)
+        index_name = result['base_name']
+        chatbot.append((parse_text("Loading vector knowledge base..."),
+                    parse_text("Success! Please tick the Vector base checkbox and ask me questions")
+                ))       
+    else:
+        chatbot.append((parse_text("Loading vector knowledge base..."),
+                    parse_text("Failed! Please contact the administrator ShuaiqiDuan, may be a bug.")
+                ))            
+    return index_name, chatbot
+
+
+def reset_user_input():
+    return gr.update(value='')
+
+
+def reset_state():
+    return [], []
+
+
+with gr.Blocks() as demo:
+    gr.HTML("""<h1 align="center">ChatGLM</h1>""")
+
+    chatbot = gr.Chatbot()
+    with gr.Row():
+        with gr.Column(scale=4):
+            with gr.Column(scale=12):
+                user_input = gr.Textbox(show_label=False, placeholder="Input...", lines=13).style(
+                    container=False)
+            with gr.Column(min_width=32, scale=1):
+                submitBtn = gr.Button("Submit", variant="primary")
+        with gr.Column(scale=1):
+            emptyBtn = gr.Button("Clear History")
+            max_length = gr.Slider(0, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
+            top_p = gr.Slider(0, 1, value=0.7, step=0.01, label="Top P", interactive=True)
+            temperature = gr.Slider(0, 1, value=0.95, step=0.01, label="Temperature", interactive=True)
+            conversation_turn = gr.Slider(5, 10, value=5, step=1, label="conversation_turn", interactive=True)
+            stream = gr.Checkbox(label="stream_out", value=True, interactive=True)
+            vector_base = gr.Checkbox(label="vector_base", value=False, interactive=True)
+            with gr.Tab(label="vector_base"):
+                File = gr.File(label="add_file",
+                    file_types=['.txt'],
+                    file_count="single",
+                    show_label=False)
+                load_file_button = gr.Button("add file and load vector knowledge base based on the file")
+    history = gr.State([])
+
+    index_name = gr.State("")
+
+    load_file_button.click(get_vector_base, [File, chatbot], [index_name, chatbot], show_progress=True)
+
+    submitBtn.click(predict, [user_input, chatbot, max_length, top_p, temperature, history, conversation_turn, stream, vector_base, index_name], [chatbot, history],
+                    show_progress=True)
+    submitBtn.click(reset_user_input, [], [user_input])
+
+
+    emptyBtn.click(reset_state, outputs=[chatbot, history], show_progress=True)
+
+demo.queue().launch(share=False, inbrowser=True)
